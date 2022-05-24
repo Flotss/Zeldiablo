9e1a7bab8ad6e833777d71b119482b81196e04e7
Florian Mangin <manginf54@gmail.com>
2022-05-24 09:02:15 +0200

    update dessinerJeu from LabbyDessin

--- zeldiablo/src/gameLaby/laby/LabyDessin.java
+++ zeldiablo/src/gameLaby/laby/LabyDessin.java
@@ -20,11 +20,11 @@ public class LabyDessin implements DessinJeu {
         LabyJeu labyJeu = (LabyJeu) jeu;
         final GraphicsContext gc = canvas.getGraphicsContext2D();
 
-        for(int i = 0 ; i < labyJeu.getLaby().murs.length ; i+=20){
-            for(int j = 0 ; j < labyJeu.getLaby().murs[i].length ; j+=20){
+        for(int i = 0 ; i < labyJeu.getLaby().murs.length ; i++){
+            for(int j = 0 ; j < labyJeu.getLaby().murs[i].length ; j++){
                 if(labyJeu.getLaby().getMur(i,j)){
                     gc.setFill(Color.BLACK);
-                    gc.fillRect(j*20, i*20, 20, 20);
+                    gc.fillRect(j*80, i*80,80, 80);
                 }
             }
         }
