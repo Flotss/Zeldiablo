8757a22cff9692213b6db488d9ebaffca100bab9
Florian Mangin <manginf54@gmail.com>
2022-05-24 08:47:59 +0200

    LabyJeu Question 11

---
+++ zeldiablo/src/gameLaby/laby/LabyJeu.java
@@ -0,0 +1,58 @@
+package gameLaby.laby;
+
+import moteurJeu.Clavier;
+import moteurJeu.Jeu;
+
+import java.io.IOException;
+
+import static gameLaby.laby.Labyrinthe.*;
+
+public class LabyJeu implements Jeu {
+
+  public static final int WIDTH = 800;
+  public static final int HEIGHT = 600;
+
+  private Labyrinthe laby;
+
+
+  public LabyJeu(String nomFichier) throws IOException {
+    this.laby = new Labyrinthe(nomFichier);
+  }
+
+  /**
+   * methode mise a jour du jeu
+   *
+   * @param secondes temps ecoule depuis la derniere mise a jour
+   * @param clavier  objet contenant l'état du clavier'
+   */
+  @Override
+  public void update(double secondes, Clavier clavier) {
+    if (clavier.haut) {
+      laby.deplacerPerso(HAUT);
+    } else if (clavier.bas) {
+      laby.deplacerPerso(BAS);
+    } else if (clavier.gauche) {
+      laby.deplacerPerso(GAUCHE);
+    } else if (clavier.droite) {
+      laby.deplacerPerso(DROITE);
+    }
+  }
+
+  /**
+   * initialisation du jeu
+   */
+  @Override
+  public void init() {
+    // pas d'initialisation particuliere
+  }
+
+  /**
+   * verifie si le jeu est fini
+   *
+   * @return booleen true si le jeu est fini
+   */
+  @Override
+  public boolean etreFini() {
+    return laby.etreFini();
+  }
+}
