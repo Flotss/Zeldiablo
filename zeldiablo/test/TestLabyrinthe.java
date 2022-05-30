77718c469975f46bebfd8b48e7acf4703bdfc5b4
Gaspard <g.bauby1@gmail.com>
2022-05-30 11:51:40 +0200

    ADD testMethodes

--- zeldiablo/test/TestLabyrinthe.java
+++ zeldiablo/test/TestLabyrinthe.java
@@ -4,50 +4,55 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class TestLabyrinthe {
-  @Test
-  public void methodes() {
-    // Initialisation
+    @Test
+    public void methodes() throws IOException {
+        // Initialisation
+        Labyrinthe laby = new Labyrinthe("labySimple/labySokoban.txt");
 
-    // Methode a teste
+        // Methode a teste
+        laby.deplacerPerso("HAUT");
+        laby.deplacerCaisse(17, 10, "HAUT");
+        laby.etreFini();
+        laby.caseDisponible(12, 10);
 
-    // Verification
-  }
-
-  @Test
-  public void constructeur() {
-
-  }
-
-
-  @Test
-  public void caseDisponible() {
-
-  }
-
-  @Test
-  public void deplacerCaisse() throws IOException {
-    // Initialisation
-    Labyrinthe laby = new Labyrinthe("labySimple/labySokoban.txt");
-
-    //Verification de la position de la caisse
-    assertTrue(laby.getCaisse(17,10));
-
-    //Deplacement de la caisse avec la methode
-    laby.deplacerCaisse(17,10,Labyrinthe.HAUT);
-
-    //Verification de la position de la caisse
-    assertTrue(laby.getCaisse(17,9));
-
-  }
+    }
 
 
-  @Test
-  public void deplacerPersoEtCaisse() {
+    @Test
+    public void constructeur() {
 
-  }
+    }
+
+
+    @Test
+    public void caseDisponible() {
+
+    }
+
+    @Test
+    public void deplacerCaisse() throws IOException {
+        // Initialisation
+        Labyrinthe laby = new Labyrinthe("labySimple/labySokoban.txt");
+
+        //Verification de la position de la caisse
+        assertTrue(laby.getCaisse(17, 10));
+
+        //Deplacement de la caisse avec la methode
+        laby.deplacerCaisse(17, 10, Labyrinthe.HAUT);
+
+        //Verification de la position de la caisse
+        assertTrue(laby.getCaisse(17, 9));
+
+    }
+
+
+    @Test
+    public void deplacerPersoEtCaisse() {
+
+    }
 
 }
