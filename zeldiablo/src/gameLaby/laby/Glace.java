1a20ff19e1f4c384b40a4c6ce36713b067d42927
Julien Bernard <julienbernardjb88@gmail.com>
2022-06-01 08:30:16 +0200

    ajout glace

---
+++ zeldiablo/src/gameLaby/laby/Glace.java
@@ -0,0 +1,19 @@
+package gameLaby.laby;
+
+import gameLaby.laby.utils.Cases;
+
+public class Glace extends Cases {
+
+    /**
+     * Constructeur de la classe Cases
+     *
+     * @param x coordonnée x
+     * @param y coordonnée y
+     */
+    public Glace(int x, int y) {
+        super(x, y);
+    }
+
+
+
+}
