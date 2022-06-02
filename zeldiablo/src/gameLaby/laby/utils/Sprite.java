73ce40f470757d36b3462a70c4848aea3636a7aa
Florian Mangin <manginf54@gmail.com>
2022-06-02 10:46:43 +0200

    Wrong path

--- zeldiablo/src/gameLaby/laby/utils/Sprite.java
+++ zeldiablo/src/gameLaby/laby/utils/Sprite.java
@@ -7,30 +7,30 @@ public class Sprite {
   /**
    * Sprite de mur
    */
-  public static Image IMAGE_MUR = new Image("file:resources/images/mur.png", 60, 60, false, false);
+  public static Image IMAGE_MUR = new Image("file:resources/mur.png", 60, 60, false, false);
 
   /**
    * Sprite de caisse
    */
-  public static Image IMAGE_CRATE = new Image("file:resources/images/crate.png", 50, 50, false, false);
+  public static Image IMAGE_CRATE = new Image("file:resources/crate.png", 50, 50, false, false);
 
   /**
    * Sprite de la glace
    */
-  public static Image IMAGE_GLACE = new Image("file:resources/images/glace.png", 60, 60, false, false);
+  public static Image IMAGE_GLACE = new Image("file:resources/glace.png", 60, 60, false, false);
 
   /**
    * Sprite du personnage
    */
-  public static Image IMAGE_PERSO = new Image("file:resources/images/personnage.png", 60, 60, false, false);
+  public static Image IMAGE_PERSO = new Image("file:resources/personnage.png", 60, 60, false, false);
 
   /**
-   *
+   * Sprite de la sortie
    */
-  public static Image IMAGE_SORTIE = new Image("file:resources/images/sortie.png", 60, 60, false, false);
+  public static Image IMAGE_SORTIE = new Image("file:resources/sortie.png", 60, 60, false, false);
 
   /**
    * Sprite de l'escalier
    */
-  public static Image IMAGE_ESCALIER = new Image("file:resources/images/escalier.png", 60, 60, false, false);
+  public static Image IMAGE_ESCALIER = new Image("file:resources/escalier.png", 60, 60, false, false);
 }
