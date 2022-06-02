2cd70d63bf585d049855a9736d7fb78a632e3a2a
Nzosim <nzosim54@gmail.com>
2022-06-02 11:35:37 +0200

    javadoc escalier

--- zeldiablo/src/gameLaby/laby/Escalier.java
+++ zeldiablo/src/gameLaby/laby/Escalier.java
@@ -1,10 +1,21 @@
 package gameLaby.laby;
 
 public class Escalier {
+    /**
+     * position en x et y de l escalier
+     */
     private int x, y;
+    /**
+     * boolean qui indique si l escalier est affiche ou non
+     */
     private boolean affiche;
 
-
+    /**
+     * constructeur de la classe escalier qui initialise sa position
+     * et indique que l escalier n est pas affiche
+     * @param x position en x de l'escalier
+     * @param y position en y de l'escalier
+     */
     public Escalier(int x, int y) {
         this.x = x;
         this.y = y;
@@ -14,8 +25,8 @@ public class Escalier {
     /**
      * Methode qui verifie si l'escalier est sur les coordonnees
      *
-     * @param x coordonnee x
-     * @param y coordonnee y
+     * @param x coordonnee en x
+     * @param y coordonnee en y
      * @return true si l'escalier est sur les coordonnees
      */
     public boolean etrePresent(int x, int y) {
