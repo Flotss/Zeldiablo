027f340846414755c8867beb41d6ee2e6f17b6fd
Florian Mangin <manginf54@gmail.com>
2022-06-02 10:37:22 +0200

    ADD Sprite

---
+++ zeldiablo/src/gameLaby/laby/utils/Sprite.java
@@ -0,0 +1,36 @@
+package gameLaby.laby.utils;
+
+
+import javafx.scene.image.Image;
+
+public class Sprite {
+  /**
+   * Sprite de mur
+   */
+  public static Image IMAGE_MUR = new Image("file:resources/images/mur.png", 60, 60, false, false);
+
+  /**
+   * Sprite de caisse
+   */
+  public static Image IMAGE_CRATE = new Image("file:resources/images/crate.png", 50, 50, false, false);
+
+  /**
+   * Sprite de la glace
+   */
+  public static Image IMAGE_GLACE = new Image("file:resources/images/glace.png", 60, 60, false, false);
+
+  /**
+   * Sprite du personnage
+   */
+  public static Image IMAGE_PERSO = new Image("file:resources/images/personnage.png", 60, 60, false, false);
+
+  /**
+   *
+   */
+  public static Image IMAGE_SORTIE = new Image("file:resources/images/sortie.png", 60, 60, false, false);
+
+  /**
+   * Sprite de l'escalier
+   */
+  public static Image IMAGE_ESCALIER = new Image("file:resources/images/escalier.png", 60, 60, false, false);
+}
